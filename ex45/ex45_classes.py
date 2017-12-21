f09bf154b603fc522339b2a5c41b1f5caf45b1f5
Dentvar <dentvar@gmail.com>
2017-12-21 21:53:18 +0100

    more work done

--- ex45/ex45_classes.py
+++ ex45/ex45_classes.py
@@ -9,41 +9,71 @@ class Player(object):
     def select_tool(self):
         print("You can choose one of the following items for your Advanture:")
         print("\nPossible tools:")
+        # List the items of the tools list
         for i in player.tools:
-                print("-", i)
-        print("What tool do you want?\n-->")
-        self.tool =(input())
-        print("before if", self.tool)
-        if self.tool == "Lighter" or "ligther": #search why its not going to the else if the input is not equal to ligher
-            print("in if", self.tool)
-        elif self.tool == "Rope":
-            print("test2")
-        else:
-            exit()
+            print("-", i)
 
+        print("What tool do you want?\n-->")
+        self.tool = str.lower((input()))
+
+        # check if input is correct
+        if self.tool != "lighter" and self.tool != "rope" and self.tool != "bottle of water":
+            print("Does not compute. Try again")
+            self.select_tool()
+        else:
+            # if input is correct just go on.
+            pass
+
+    def select_weapon(self):
+        print("You can choose one of the following Weapons:")
+        print("\nPossible Weapons:")
+
+        # List the items of the weapons list
+        for i in player.weapons:
+            print("-", i)
+
+        print("What Weapon do you choose?\n-->")
+        self.weapon = str.lower((input()))
+
+        # check if input is correct
+        if self.weapon != "sword" and self.weapon != "crossbow" and self.weapon != "shield" and self.weapon != "spear":
+            print("Does not compute. Try again")
+            self.select_weapon()
+        else:
+            # if input is correct just go on.
+            pass           
     
     tools={
-        "Lighter",
-        "Rope",
-        "Bottle_of_water"
+        "lighter",
+        "rope",
+        "bottle of water"
     }
 
     weapons={
-        "Sword",
-        "Crossbow",
-        "Shield",
-        "Spear"
+        "sword",
+        "crossbow",
+        "shield",
+        "spear"
     }
 
 class Room(object):
     pass
+    # rooms={
+    #     "tunnel": tunnel(),
+    #     "first room": tunnel(),
+    #     "pit": tunnel(),
+    #     "diamond room": tunnel(),
+    #     "dark dunnel": tunnel(),
+    #     "pool": tunnel(),
+    #     "arena": tunnel(),
+    #     "staircase": tunnel()
+    # }
 
 class Tunnel(Room):
 
     def enter(self):
-        print("You are in Room 'Tunnel'")
-        print(player.tool)
-        exit()
+        print("You are in Room 'tunnel'")
+        play("first room")
 
 class firstRoom(Room):
     def enter(self):
@@ -52,66 +82,68 @@ class firstRoom(Room):
 
 class Pit(Room):
     def enter(self):
-        print("You are in Room:'Pit'")
+        print("You are in Room:'pit'")
         exit()
     
 class DiamondRoom(Room):
     def enter(self):
-        print("You are in Room:'DiamondRoom'")
+        print("You are in Room:'diamondroom'")
         exit()
 
 class DarkTunnel(Room):
     def enter(self):
-        print("You are in Room:'DarkTunnel'")
+        print("You are in Room:'darktunnel'")
         exit()
 
 class Pool(Room):
     def enter(self):
-        print("You are in Room:'Pool'")
+        print("You are in Room:'pool'")
         exit()
 
 class Arena(Room):
     def enter(self):
-        print("You are in Room:'Arena'")
+        player.select_weapon()
+        print("You are in Room:'arena'")
         exit()
 
 class Staircase(Room):
     def enter(self):
-        print("You are in Room:'Staircase'")
+        print("You are in Room:'staircase'")
         exit()
 
-class Engine(object):
-    
-    def play(self, next_room):
-        if next_room == "Tunnel":
+#Other funtions
+
+def play(next_room):
+
+        if next_room == "tunnel":
             current_room = Tunnel()
             current_room.enter()
 
-        elif next_room == "firstRoom":
+        elif next_room == "first room":
             current_room = firstRoom()
             current_room.enter()
 
-        elif next_room == "Pit":
+        elif next_room == "pit":
             current_room = Pit()
             current_room.enter()
 
-        elif next_room == "DiamondRoom":
+        elif next_room == "diamond room":
             current_room = DiamondRoom()
             current_room.enter()
 
-        elif next_room == "DarkTunnel":
+        elif next_room == "dark tunnel":
             current_room = DarkTunnel()
             current_room.enter()
 
-        elif next_room == "Pool":
+        elif next_room == "pool":
             current_room = Pool()
             current_room.enter()
 
-        elif next_room == "Arena":
+        elif next_room == "arena":
             current_room = Arena()
             current_room.enter()
 
-        elif next_room == "Staircase":
+        elif next_room == "staircase":
             current_room = Staircase()
             current_room.enter()
 
@@ -120,31 +152,11 @@ class Engine(object):
             exit()
 
 
-
-#Other funtions
-
-
-    
-
 #Debug and test funtions
-
-def show_tools(player):
-    print("\nPossible tools:")
-    for i in player.tools:
-        print("-", i)
-
-def show_weapons(player):
-    print("\nPossible weapons:")
-    for i in player.weapons:
-        print("-", i)
 
 
 #Main
 player = Player(input("Hello Advanturer, whats your Name?\n-->"))
 print("Welcome",player.name)
 player.select_tool()
-game = Engine()
-game.play("Tunnel")
-
-# test = Tunnel()
-# test.enter()
+play("tunnel")
